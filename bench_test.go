bce0b544dbf3fdc54d87f8906640c8380e34a5ed
zam <zamzam.saeful@efishery.com>
2023-11-30 04:25:22 +0700

    test: update bench

--- bench_test.go
+++ bench_test.go
@@ -1,24 +1,260 @@
 package lcs_go
 
-import "testing"
+import (
+	"testing"
+)
 
 func BenchmarkDp(t *testing.B) {
-	for i := 0; i < t.N; i++ {
-		dp("COMPUTER", "SCIENCE")
+	tests := []struct {
+		name string
+		a    string
+		b    string
+		want string
+	}{
+		{
+			name: "should return empty string",
+			a:    "",
+			b:    "",
+			want: "",
+		},
+		{
+			name: "should return empty string",
+			a:    "A",
+			b:    "",
+			want: "",
+		},
+		{
+			name: "should return empty string",
+			a:    "",
+			b:    "A",
+			want: "",
+		},
+		{
+			name: "should return CE",
+			a:    "COMPUTER",
+			b:    "SCIENCE",
+			want: "CE",
+		},
+		{
+			name: "should return ACE",
+			a:    "ABCDE",
+			b:    "ACE",
+			want: "ACE",
+		},
+		{
+			name: "should return ABC",
+			a:    "ABC",
+			b:    "ABC",
+			want: "ABC",
+		},
+		{
+			name: "should return FSH",
+			a:    "FOSH",
+			b:    "FISH",
+			want: "FSH",
+		},
+		{
+			name: "should return FO",
+			a:    "FOSH",
+			b:    "FORT",
+			want: "FO",
+		},
+		{
+			name: "should return A CE",
+			a:    "AB CE",
+			b:    "A CE",
+			want: "A CE",
+		},
+		{
+			name: "should return MU",
+			a:    "ILMU",
+			b:    "KOMPUTER",
+			want: "MU",
+		},
+		{
+			name: "should return MU",
+			a:    "ILMUO",
+			b:    "KOMPUTER",
+			want: "MU",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.B) {
+			for i := 0; i < t.N; i++ {
+				dp(test.a, test.b)
+			}
+		})
 	}
 
 }
 
 func BenchmarkRecursive(t *testing.B) {
-	for i := 0; i < t.N; i++ {
-		recursive("COMPUTER", "SCIENCE")
+	tests := []struct {
+		name string
+		a    string
+		b    string
+		want string
+	}{
+		{
+			name: "should return empty string",
+			a:    "",
+			b:    "",
+			want: "",
+		},
+		{
+			name: "should return empty string",
+			a:    "A",
+			b:    "",
+			want: "",
+		},
+		{
+			name: "should return empty string",
+			a:    "",
+			b:    "A",
+			want: "",
+		},
+		{
+			name: "should return CE",
+			a:    "COMPUTER",
+			b:    "SCIENCE",
+			want: "CE",
+		},
+		{
+			name: "should return ACE",
+			a:    "ABCDE",
+			b:    "ACE",
+			want: "ACE",
+		},
+		{
+			name: "should return ABC",
+			a:    "ABC",
+			b:    "ABC",
+			want: "ABC",
+		},
+		{
+			name: "should return FSH",
+			a:    "FOSH",
+			b:    "FISH",
+			want: "FSH",
+		},
+		{
+			name: "should return FO",
+			a:    "FOSH",
+			b:    "FORT",
+			want: "FO",
+		},
+		{
+			name: "should return A CE",
+			a:    "AB CE",
+			b:    "A CE",
+			want: "A CE",
+		},
+		{
+			name: "should return MU",
+			a:    "ILMU",
+			b:    "KOMPUTER",
+			want: "MU",
+		},
+		{
+			name: "should return MU",
+			a:    "ILMUO",
+			b:    "KOMPUTER",
+			want: "MU",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.B) {
+			for i := 0; i < t.N; i++ {
+				recursive(test.a, test.b)
+			}
+		})
 	}
 
 }
 
 func BenchmarkBacktracking(t *testing.B) {
-	for i := 0; i < t.N; i++ {
-		backtracking("COMPUTER", "SCIENCE")
+	tests := []struct {
+		name string
+		a    string
+		b    string
+		want string
+	}{
+		{
+			name: "should return empty string",
+			a:    "",
+			b:    "",
+			want: "",
+		},
+		{
+			name: "should return empty string",
+			a:    "A",
+			b:    "",
+			want: "",
+		},
+		{
+			name: "should return empty string",
+			a:    "",
+			b:    "A",
+			want: "",
+		},
+		{
+			name: "should return CE",
+			a:    "COMPUTER",
+			b:    "SCIENCE",
+			want: "CE",
+		},
+		{
+			name: "should return ACE",
+			a:    "ABCDE",
+			b:    "ACE",
+			want: "ACE",
+		},
+		{
+			name: "should return ABC",
+			a:    "ABC",
+			b:    "ABC",
+			want: "ABC",
+		},
+		{
+			name: "should return FSH",
+			a:    "FOSH",
+			b:    "FISH",
+			want: "FSH",
+		},
+		{
+			name: "should return FO",
+			a:    "FOSH",
+			b:    "FORT",
+			want: "FO",
+		},
+		{
+			name: "should return A CE",
+			a:    "AB CE",
+			b:    "A CE",
+			want: "A CE",
+		},
+		{
+			name: "should return MU",
+			a:    "ILMU",
+			b:    "KOMPUTER",
+			want: "MU",
+		},
+		{
+			name: "should return MU",
+			a:    "ILMUO",
+			b:    "KOMPUTER",
+			want: "MU",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.B) {
+			for i := 0; i < t.N; i++ {
+				backtracking(test.a, test.b)
+			}
+		})
 	}
 
 }
